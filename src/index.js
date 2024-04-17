3510da62a651c4526d7e38025a15ecb9778c5794
FE_Wani <62400479+baesee0806@users.noreply.github.com>
2024-04-17 13:29:58 +0900

    feat : community, view, list, search router 로직 구현
    - community (배성완)
    - list (김원준)
    - search (박진양)
    
    Relate to :#2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bookReport from "./router/community/bookReport.js";
+import community from "./router/community/community.js";
+import view from "./router/community/view.js";
+import list from "./router/list.js";
+import search from "./router/search.js";
 
 dotenv.config();
 
 const app = express();
-const port = 8080;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: true }));
 
-app.listen(port);
+app.listen(process.env.PORT);
 
-app.use("/community", bookReport);
+app.use("/community", community);
+app.use("/view", view);
+app.use("/list", list);
+app.use("/search", search);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
