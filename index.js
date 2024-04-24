c4f154be50702946fdfc99f7b26b51368190ade9
FE_Wani <62400479+baesee0806@users.noreply.github.com>
2024-04-24 14:09:34 +0900

    node  error

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: true }));
 
+const server = app.listen(3000);
+server.keepAliveTimeout = 61 * 1000;
+server.headersTimeout = 65 * 1000;
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
